7547a77335706ae18839aac3b0606aadf54ecfa9
norbertpaluczynski <norbert.paluczynski@gmail.com>
2021-10-30 16:43:16 +0200

    liquibase db init

--- nmcookbook-api/app.js
+++ nmcookbook-api/app.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+const Liquibase = require('node-liquibase').Liquibase;
+const POSTGRESQL_DEFAULT_CONFIG = require('node-liquibase').POSTGRESQL_DEFAULT_CONFIG;
+
+const myConfig = {
+  ...POSTGRESQL_DEFAULT_CONFIG,
+  changeLogFile: './resources/db/liquibase/changeLog.xml',
+  url: 'jdbc:postgresql://localhost:5432/nmcookbook',
+  username: 'nmcookbook',
+  password: 'nmcookbook',
+}
+const instTs = new Liquibase(myConfig);
+
+instTs.update();
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
