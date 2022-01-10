2fce44412b3b4cb59f3c88dadb82f2b8a47a26c6
Michał Ratajczak <michalratajczak2105@gmail.com>
2022-01-10 18:42:28 +0100

    recipe details page

--- nmdrinkbook-front/pages/drink/_id.vue
+++ nmdrinkbook-front/pages/drink/_id.vue
@@ -2,8 +2,57 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="12" md="12" lg="8">
       <v-card>
-        <v-card-text>
-
+        <v-img :src="recipe.image" height="100%"/>
+        <v-container class="pb-0">
+          <v-row>
+            <v-col>
+              <div class="d-flex justify-space-between">
+                <v-card-title>{{ recipe.title }}</v-card-title>
+                <div class="d-flex align-center px-4">
+                  <TextIcon icon='mdi-eye-outline' :text="`${recipe.views}`" :tooltip="$t('tooltips.views')"/>
+                  <TextIcon icon='mdi-finance' :text="`${recipe.difficultyLevel}`" :tooltip="$t('tooltips.difficultyLevel')"/>
+                  <TextIcon icon='mdi-clock-outline' :text="`${recipe.preparationTime}`" :tooltip="$t('tooltips.preparationTime')"/>
+                  <TextIcon v-if="recipe.rating" icon='mdi-star' :text="`${recipe.rating}`" :tooltip="$t('tooltips.rating')"/>
+                </div>
+              </div>
+              <v-card-subtitle class="pt-0">
+                <template v-for="category in recipe.categories">
+                  <v-chip
+                    :key="category.categoryId"
+                    outlined small
+                    class="mr-2"
+                  >{{ category.name }}</v-chip>
+                </template>
+              </v-card-subtitle>
+            </v-col>
+          </v-row>
+        </v-container>
+        <v-card-text class="pt-0">
+          <v-container>
+            <v-row>
+              <v-col>
+                {{ recipe.description }}
+              </v-col>
+            </v-row>
+            <v-row class="mt-6">
+              <v-col cols="12" class="col-md-6">
+                <h3 class="text-h6 mb-3">Sposób przygotowania:</h3>
+                <div>{{ recipe.preparationSteps }}</div>
+              </v-col>
+              <v-col cols="12" class="col-md-5 offset-md-1">
+                <h3 class="text-h6">Składniki:</h3>
+                <v-list>
+                  <template v-for="(ingredient, index) in recipe.ingredients">
+                    <v-list-item :key="ingredient.ingredientId">
+                      <v-list-item-title>{{ ingredient.articleName }}</v-list-item-title>
+                      <v-list-item-subtitle class="text-right">{{ ingredient.quantity }} {{ ingredient.symbol }}</v-list-item-subtitle>
+                    </v-list-item>
+                    <v-divider :key="index" v-if="index < recipe.ingredients.length" />
+                  </template>
+                </v-list>
+              </v-col>
+            </v-row>
+          </v-container>
         </v-card-text>
       </v-card>
     </v-col>
@@ -38,7 +87,8 @@ export default Vue.extend({
     }
 
     return {
-      getData
+      getData,
+      recipe
     }
   }
 })
