3050d6d1af8d4c7c5402dbaa465a54f49788877c
Norbert Pałuczyński <norbert.paluczynski@gmail.com>
2022-01-15 17:36:42 +0100

    secure article, category, unit endpoints

--- nmdrinkbook-api/controllers/unit.controller.js
+++ nmdrinkbook-api/controllers/unit.controller.js
@@ -1,4 +1,5 @@
 import Unit from '../models/unit.model.js'
+import jwt_decode from 'jwt-decode'
 
 export const unitController = () => {
     const create = (req, res) => {
@@ -9,66 +10,95 @@ export const unitController = () => {
         //     return;
         // }
     
-        const unit = {
-            symbol: req.body.symbol,
-            createdBy: req.createdBy,
-            modifiedBy: req.modifiedBy
-        }
-    
-        Unit.create(unit)
-            .then(data => {
-                res.send(data);
-            })
-            .catch(err => {
-                res.status(500).send({
-                    message: err.message || "Unknown error."
+        var authHeader = req.headers.authorization
+        
+        var token = authHeader.substring(7, authHeader.length)
+        var decoded = jwt_decode(token)
+        let is_admin = decoded['resource_access']['nmclient']['roles'].includes('admin')
+
+        if (!is_admin) {
+            res.status(401).send()
+        } else {
+            const unit = {
+                symbol: req.body.symbol,
+                createdBy: req.createdBy,
+                modifiedBy: req.modifiedBy
+            }
+        
+            Unit.create(unit)
+                .then(data => {
+                    res.send(data);
+                    
                 })
-            });
+                .catch(err => {
+                    res.status(500).send({
+                        message: err.message || "Unknown error."
+                    })
+                });
+        }
     };
 
     const save = (req ,res) => {
         const id = req.params.id
         req.body.modifiedBy = req.modifiedBy
+        var authHeader = req.headers.authorization
+        
+        var token = authHeader.substring(7, authHeader.length)
+        var decoded = jwt_decode(token)
+        let is_admin = decoded['resource_access']['nmclient']['roles'].includes('admin')
 
-        Unit.update(req.body, {
-            where: { unitId: id }
-        })
-            .then(num => {
-                if (num == 1) {
-                    res.send(req.body)
-                } else {
-                    res.status(500).send({
-                        message: "error"
-                    })
-                }
+        if (!is_admin) {
+            res.status(401).send();
+        } else {
+            Unit.update(req.body, {
+                where: { unitId: id }
             })
-            .catch(err => {
-                res.status(500).send({
-                    message: "error catch"
+                .then(num => {
+                    if (num == 1) {
+                        res.send(req.body)
+                    } else {
+                        res.status(500).send({
+                            message: "error"
+                        })
+                    }
                 })
-            })
+                .catch(err => {
+                    res.status(500).send({
+                        message: "error catch"
+                    })
+                })
+        }
     }
 
     const remove = (req, res) => {
         const id = req.params.id
+        var authHeader = req.headers.authorization
+        
+        var token = authHeader.substring(7, authHeader.length)
+        var decoded = jwt_decode(token)
+        let is_admin = decoded['resource_access']['nmclient']['roles'].includes('admin')
 
-        Unit.destroy({
-            where: { unitId: id }
-        })
-            .then(num => {
-                if (num == 1) {
-                    res.status(204).send();
-                } else {
-                    res.send({
-                        message: `error`
-                    });
-                }
+        if (!is_admin) {
+            res.status(401).send()
+        } else {
+            Unit.destroy({
+                where: { unitId: id }
             })
-            .catch(err => {
-                res.status(500).send({
-                message: "error catch"
-                });
-          });
+                .then(num => {
+                    if (num == 1) {
+                        res.status(204).send();
+                    } else {
+                        res.send({
+                            message: `error`
+                        });
+                    }
+                })
+                .catch(err => {
+                    res.status(500).send({
+                    message: "error catch"
+                    });
+              });
+        }
     }
 
     const findById = (req, res) => {
